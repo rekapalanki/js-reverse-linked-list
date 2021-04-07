0804ff8409792d954dacb23d20134c5a7390e5ee
Reka Palanki <reka.palanki@gmail.com>
2021-04-07 12:23:25 +0100

    adding class and elements

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Reverse - it works!");
+class LinkedListNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+const a = new LinkedListNode(5);
+const b = new LinkedListNode(10);
+const c = new LinkedListNode(12);
+const d = new LinkedListNode(33);
+const e = new LinkedListNode(400);
+
